4315bb510dee5bbd6b875585a0c10de1423b64c2
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-05-14 18:28:55 +0300

    Update code

---
+++ src/Api/Api.js
@@ -0,0 +1,2 @@
+const BASE_URL = 'https://pixabay.com/api/?q=&page=1&key=&image_type=photo&orientation=horizontal&per_page=12';
+const API_KEY = '34887876-89bff92d754edb3a579b9cb65';
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import { StyledApp } from "./App.styled";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledApp>
+    </StyledApp>
   );
 };
